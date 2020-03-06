bbb3391afa503c304348d6e2578da2fce5e5e410
meschial <marcosmeschial@gmail.com>
2020-03-06 19:46:41 -0300

    first commit

---
+++ offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1583534616,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/bg-sheet0.png",
+		"images/chão-sheet0.png",
+		"images/base-sheet0.png",
+		"images/canhão-sheet0.png",
+		"images/caixas-sheet0.png",
+		"images/bullet-sheet0.png",
+		"images/particles.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
